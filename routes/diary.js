bb76be120beaec1dd3954da7bb79ac7405d44fb2
jjanddak <opplane@gmail.com>
2020-12-24 14:34:47 +0900

    [Server]/#9/edit:diaryController.js routes/diary.js 댓글과 좋아요 삭제 미구현

--- routes/diary.js
+++ routes/diary.js
@@ -6,6 +6,10 @@ const diaryController = require('../controllers/diaryController');
 //POST diary/newPost
 router.post('/newPost', diaryController.newPost);
 
+//POST diary/updatePost
 router.post('/updatePost', diaryController.updatePost);
+
+//POST diary/deletePost
+router.post('/deletePost', diaryController.deletePost);
 
 module.exports = router;
