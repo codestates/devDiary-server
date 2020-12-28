489db38c2f5082f577c1c9cbda548f2fd1aa1b8b
Choe-Yun <cjh7687@gmail.com>
2020-12-28 11:20:40 +0900

    [server]/#17/edit:controllers/index.js (칼럼참조 미완성)

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,11 @@
 const { User } = require("../models");
+const { diarie } = require("../models");
+const { like } = require("../models");
+const { question } = require("../models")
+const { comment } = require("../models")
 
 module.exports = {
-	login: async (req, res) => {
+  login: async (req, res) => {
 		const body = req.body;
 		const userinfo = await User.findOne({
 			where: {
@@ -18,5 +22,32 @@ module.exports = {
 				res.status(200).json({id : userinfo.id, email : body.email,username : userinfo.username});
 			});
 		}	
-	},
+  },
+  // User 와 연결된 테이블,
+  // diarie 와 연결된 테이블 include
+  // question 와 연결된 테이블 include
+  // like 와 연결된 테이블 include
+  // comment 와 연결된 테이블 include
+  
+	getuserinfo: async (req, res) => {
+    
+		const userinfo = await User.findAll({
+      // where : {id : req.session.userid},
+      include : [{
+        model : diarie,
+        attributes : ["id","title","createdAt"],
+    },
+  {
+    model : question,
+    attributes : ["id","title","createdAt"],
+      
+  }]
+
+    }).catch(err => {console.log(err)})
+    if(!userinfo){
+      res.status(404).send("not found")
+    } else if(userinfo){
+      res.status(200).json(userinfo)
+    }
+	}
 };
