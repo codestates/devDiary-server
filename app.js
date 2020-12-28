9e38b7f730c1c643c86bd7e918c57599ec8da295
jjanddak <opplane@gmail.com>
2020-12-28 18:23:36 +0900

    duplicated cors edited & merged

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const cors=require("cors");
 const bodyParser = require("body-parser");
 const session = require("express-session");
 const diaryRouter = require('./routes/diary');
-// const userRouter = require('./routes/user');
+const userRouter = require('./routes/user');
 
 const port = 4000;
 const app=express();
 
-app.use(cors());
+// app.use(cors());
 app.use(bodyParser.json());
 
 
@@ -36,7 +36,7 @@ app.use(express.json());
 // app.post("/user/checkEmail", mainController.filteremail);
 // app.post("/user/checkUsername", mainController.filterusername);
 
-// app.use('/user', usersRouter);
+app.use('/user', userRouter);
 app.use('/diary', diaryRouter);
 
 if (process.env.NODE_ENV !== "test") {
