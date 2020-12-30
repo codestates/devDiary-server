775b63805c77af38fb17b2172be50bf414e563b2
Choe-Yun <cjh7687@gmail.com>
2020-12-30 11:30:02 +0900

    [server]/#35/feature:패스워드체크

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,57 +1,15 @@
 const { User, diarie, like, question, comment } = require("../models");
-const sequelize = require("sequelize");
 
 module.exports = {
-  	getuserinfo: async (req, res) => {
-    
-    const userinfo = await User.findOne({
-      attributes: ["id","email","username"],
-      include : [{
-        model : diarie,
-        attributes : ["id","title","createdAt"],
-        include : [{
-          model : comment,
-          attributes : [[sequelize.fn("COUNT","diary_id"), "commentCount"]]
-        },{
-          model : like,
-          attributes : [[sequelize.fn("COUNT","diary_id"), "diarieLikeCount"]]
-        }]
-    },
-    {
-      model : like,
-      attributes : [[sequelize.fn('COUNT','user_id'), 'userLikeCount']]
-    },
-  {
-    model : question,
-    attributes : ["id","title","createdAt"],
-    include : [{
-      model : comment,
-      attributes : ["question_id"]
-    },{
-      model : like,
-      attributes : [[sequelize.fn("COUNT", "question_id"), "questionLikeCount"]]
-    }]
-  }
-]}).catch(err => {console.log(err)})
-    if(!userinfo){
-      res.status(404).send("not found")
-    } else if(userinfo){
-      res.status(200).json(userinfo)
-    }
-  },
-  deleteUser = async (req,res) =>{
+  CheckPassWord : async (req,res) => {
     const body = req.body
-    const deleteUserInfo = await User.findOneAndDelete({
-      where : {
-        email : body.email,
-        username : body.username,
-        password : body.password
-      }
-    }).catch(err => {console.log(err)})
-    if(!deleteUserInfo){
-      res.status(404).send({message : "돌아가"})
-    } else if(deleteUserInfo){
-      res.status(200).json(deleteUserInfo)
+    const check = await User.findOne({
+      where : body.password
+    })
+    if(!check){
+      res.status(422).send({message : "PassWord Not Same"})
+    } else {
+      res.status(200).send({message : "PassWord Same"})
     }
   }
 };
--- routes/user.js
+++ routes/user.js
@@ -3,13 +3,7 @@ const router = express.Router();
 
 const userController = require("../controllers/userController");
 
-//Get user/userinfo
-// router.get("/userinfo", userController.getuserinfo)
-
-//Post user/deleteUser
-// router.post("/deleteUser", userController.deleteUser)
-
 //Post user/CheckPassWord
-router.post("CheckPassWord", userController.CheckPassWord)
+router.post("/checkPassword", userController.CheckPassWord)
 
 module.exports = router;
