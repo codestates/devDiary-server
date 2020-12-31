66b4e48f11f8a983f13bd953450301ce3f7c4d7b
Choe-Yun <cjh7687@gmail.com>
2020-12-31 14:08:27 +0900

    [Server]/#46/feature:회원탈퇴기능 수정

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,18 +2,16 @@ const { User, diarie, like, question, comment } = require("../models");
 const sequelize = require("sequelize");
 module.exports = {
   deleteUser : async (req,res) =>{
-    const body = req.body
+    const sess = req.session
     const deleteUserInfo = await User.destroy({
       where : {
-        email : body.email,
-        username : body.username,
-        password : body.password
+        username : sess.username
       }
     }).catch(err => {console.log(err)})
     if(!deleteUserInfo){
       res.status(404).send({message : "Cant Delete"})
     } else if(deleteUserInfo){
-      res.status(200).json(deleteUserInfo)
+      res.status(200).send({message : "bye~"})
     }
   },
   
